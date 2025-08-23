567a8079daa79078d01c2d61d881e7095a5563a5
samuel <samuelconnde@gmail.com>
2025-08-23 19:26:48 -0300

    Subindo versão atualizada

--- js/script.js
+++ js/script.js
@@ -1,30 +1,15 @@
 // Animação para o ícone das linguagens de programação
-document.querySelectorAll('.icones').forEach(img => {
-    // Evento para quando o mouse ficar em cima do ícone
+// Pegando todos os ícones da classe icones
+const icones = document.querySelectorAll('.icones');
+
+icones.forEach(img => {
     img.addEventListener('mouseenter', () => {
-        img.style.transform = 'scale(1.2)'; // Mundando o tamanho da imagem quando passa o mouse por cima
-
-        let textoExibicao = img.getAttribute('title'); // Pegando o título da imagem para exibição
-
-        // Criando um ToolTip
-        let tooltip = document.createElement('div');
-        tooltip.textContent  = textoExibicao; // Colocando texto no tooltip
-        tooltip.classList.add('tooltip-temp');
-
-        tooltip.style.color = 'white';
-        tooltip.style.padding = '5px';
-        tooltip.style.borderRadius = '5px';
-        tooltip.style.fontSize = '12px';
-
-        document.body.appendChild(tooltip);
+        //aumenta o ícone quando o mouse fica em cima
+        img.style.transform = 'scale(1.1)';
     });
 
-    // Evento para quando o mouse sair de cima do ícone
     img.addEventListener('mouseleave', () => {
-        img.style.transform = 'scale(1)'; // volta ao normal
-        
-        // Remove o tooltip
-        document.querySelectorAll('.tooltip-temp').forEach(t => t.remove());
+        img.style.transform = 'scale(1)';
     });
 });
 
